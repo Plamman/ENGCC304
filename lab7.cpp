c224afff4f504034bc1c76bb5ec86d10ef1b89d5
Plamman <sunsurn_so67@live.rmutl.ac.th>
2024-09-06 22:08:24 +0700

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -54,54 +54,62 @@
 #include <time.h>
 
 int main() {
-    int t , p , s , h , l ;
+    int t , p , s  ;
+    int h = 1 ;
+    int l = 100 ;
     int score = 100 ;
-  
-
-    printf ("Do you want to play game (1=play,-1=exit) =") ;
-    scanf ("%d", &p) ;
+do{
+     printf ( "Do you want to play game (1=play,-1=exit) =" ) ; //คำสั้งเริ่มเกมแล้วรับค่า p
+     scanf ( "%d" , & p ) ;
     
-    if ( p == 1) {
-        srand ( time( NULL ) ) ;
-        t = rand () % 101 ;
-        printf ("%d\n", t ) ;
+  if ( p == 1 ) {
+    srand ( time ( NULL ) ) ; // การวน แล้วเก็บค่า t 
+    t = rand () % 101 ;
+    printf ( "%d\n" , t ) ;
 
-        printf ("\nplay\n") ; 
-        printf ("\nscore=%d\n",score ) ; 
-        printf ("Guess the winning number (1-100)  =") ;     
-        scanf("%d",&s) ;
+    printf ( "\nplay\n" ) ; 
+    printf ( "\nscore=%d\n" , score ) ; //แสดงข้อมูล score
         
         
-        while (1) {
+        
+      while ( 1 ) {
+        printf ( "Guess the winning number (1-100)  =" ) ;     
+        scanf( "%d", & s ) ; //รับค่าจากผู้เล่น ค่าคือ s
+          
             
-         if( t > s ) {
-                printf ("Sorry, the winning number is HIGHER than %d\n",s) ;
-                score = score - 10 ;
-                printf ("Score= %d\n",score) ;
-                h = s + 1 ;
-                printf ("Guess the winning number (%d-100)\n",h) ;
-              }else if ( t < s ){
-                printf ("Sorry, the winning number is LOWER than %d\n",s) ;
-                score = score - 10 ;
-                printf ("Score= %d\n",score) ;
-                l = s + 1 ;
-                h = s - 1 ;
-                printf ("Guess the winning number (%d-%d)\n", h , l ) ;
-              }else {
-                 printf("That is correct! The winning number is %d\n",s) ;
-                 printf("Score this game: %d\n",score) ;
-                 break; 
-                {
+         if( t > s ) { //ถ้าค่าของ t การสุ่ม มากกว่า ค่าของ sค่าของข้อมูลการผู้เล่น
+            printf ( "Sorry, the winning number is HIGHER than %d\n" , s ) ;
+            score = score - 10 ;
+            printf ( "Score= %d\n" , score ) ;
+             h = s + 1 ;
+            printf ( "Guess the winning number (%d-%d)\n" , h  , l ) ;
+              
+         } else if ( t < s ) {
+            printf ( "Sorry, the winning number is LOWER than %d\n" , s ) ;
+            score = score - 10 ;
+            printf ( "Score= %d\n" , score ) ;
+            l = s + 1 ;
+            printf ( "Guess the winning number (%d-%d)\n" , h , l ) ;
+         } else {
+            printf( "That is correct! The winning number is %d\n", s ) ;
+            printf( "Score this game: %d\n" , score ) ;
             
-              }
+         } 
+         if ( score == 0 ) {
+            printf ( "\nexit" ) ;
+            printf ( "Are you Goimg to continue playing (1=play,-1=exit) =" ) ;
+            scanf ( "%d" , & s ) ;
+              
             }
-        }
+       
+      }
                        
 
-    }else if( p == -1 ) {
-        printf ("exit") ;
-    }else{
-        printf("error") ;
-    }
-    return 0 ;
+    } else if ( p == -1 ) {
+        printf ( "exit" ) ;
+    } else { //end else if
+        printf ( "error" ) ;
+    }//end else
+}while (1 != 1) ;
+  return 0 ;
 }//end main function
