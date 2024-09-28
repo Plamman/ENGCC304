27a59fb7198a023ed8ba2edd9be69cd99a9d56ce
Plamman <sunsurn_so67@live.rmutl.ac.th>
2024-09-28 14:48:24 +0700

    Create lab11.cpp

---
+++ lab11.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+int Separatenumbers(int number) {
+
+    int sum = 0 ;
+    int digit ;
+    int originalNumber = number ;
+
+    while (number > 0) {
+        digit = number % 10 ;
+        sum += digit * digit * digit ;
+        number = number / 10 ;
+    }
+
+
+    if (sum == originalNumber) {
+        printf ("Pass") ;
+    } else {
+        printf ("Not Pass") ;
+    }
+
+    return 0 ;
+}
+
+int main() {
+    int number ;
+    printf ("Enter a number: ") ;
+    scanf ("%d", &number) ; 
+    Separatenumbers (number) ;
+    
+    return 0 ;
+}
