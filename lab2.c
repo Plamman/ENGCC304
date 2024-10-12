1c7aba9c6c5af3c62e38830aab2d0697f6258ad2
Plamman <sunsurn_so67@live.rmutl.ac.th>
2024-10-12 21:46:00 +0700

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char character ;
+    int num1 , num2 ;
+    float floatingPoint; 
+    char string[ 100 ] ;
+
+    printf ("Enter a character: ") ; 
+    scanf (" %c", &character) ; 
+    printf ("Enter two integers: ") ;
+    scanf ("%d %d", &num1, &num2) ;
+    printf ("Enter a floating-point number: ") ;
+    scanf ("%f", &floatingPoint) ;
+    printf ("Enter a string: ") ;
+    scanf ("%s", string) ;
+
+    printf ("\nOutput:\n") ;
+    printf ("You entered character: %c\n", character) ;
+    printf ("You entered integers: %d and %d\n", num1, num2) ;
+    printf ("You entered floating-point number: %.2f\n", floatingPoint) ; 
+    printf ("You entered string: %s \n", string) ;
+
+    return 0;
+}
